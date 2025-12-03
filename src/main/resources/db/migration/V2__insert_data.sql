b7cad509440e35f88589cbb4419eebda71ae59cd
Madik981 <madi12312456@gmail.com>
2025-12-03 09:13:07 +0500

    added more data to migration

--- src/main/resources/db/migration/V2__insert_data.sql
+++ src/main/resources/db/migration/V2__insert_data.sql
@@ -6,8 +6,11 @@ INSERT INTO t_country (code, country) VALUES ('KR', 'South Korea');
 INSERT INTO t_country (code, country) VALUES ('KZ', 'Kazakhstan');
 
 INSERT INTO t_item (name, description, price, category_id) VALUES ('iPhone 15 Pro', 'Powerful flagship smartphone', 639980, 1);
+INSERT INTO t_item (name, description, price, category_id) VALUES ('Galaxy S25 Ultra', 'High-end Android smartphone', 500000, 1);
 INSERT INTO t_item (name, description, price, category_id) VALUES ('HyperX Cloud II', 'Popular Gaming Headphones', 30000, 2);
 
 INSERT INTO t_item_countries (item_id, countries_id) VALUES (1, 1);
 INSERT INTO t_item_countries (item_id, countries_id) VALUES (1, 3);
 INSERT INTO t_item_countries (item_id, countries_id) VALUES (2, 2);
+INSERT INTO t_item_countries (item_id, countries_id) VALUES (2, 3);
+INSERT INTO t_item_countries (item_id, countries_id) VALUES (3, 1);
